de470a7fe753179d05e7a42ff35aeb9d09736f8c
‘ZhengHongyi’ <1111@163.com>
2023-08-23 11:30:20 +0800

    在web中使用mybatis 环境搭建

---
+++ zhy-mybatis/zhy-mybatis-web/src/main/java/com/jdjm/pojo/account.java
@@ -0,0 +1,19 @@
+package com.jdjm.pojo;
+
+import lombok.Data;
+
+/**
+ * @version v1.0
+ * @Author zhy
+ * Description 默认
+ * @Date 2023-08-23-11:28:13
+ */
+
+@Data
+public class account {
+    private Long id;
+
+    private String accountNo;
+
+    private Double balance;
+}

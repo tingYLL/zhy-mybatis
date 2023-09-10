423c0206ac16ea4d338d52b143d40725ea362b59
‘ZhengHongyi'’ <1111@163.com>
2023-09-10 13:01:54 +0800

    动态sql foreach、set、choose

--- zhy-mybatis/zhy-mybatis-dynamic-sql/src/main/java/com/jdjm/mapper/DynamicSqlMapper.java
+++ zhy-mybatis/zhy-mybatis-dynamic-sql/src/main/java/com/jdjm/mapper/DynamicSqlMapper.java
@@ -11,6 +11,21 @@ public interface DynamicSqlMapper {
 
     public List<SysRole> queryRole(@Param("vo") SysRoleDTO sysRoleDTO);
 
-
     public List<SysRole> queryRoleByTrim(@Param("vo") SysRoleDTO sysRoleDTO);
+
+
+//    测试动态sql的set标签的使用
+    public Boolean updateBySet(@Param("vo")SysRole sysRole);
+
+//    测试choose标签的使用
+    public List<SysRole> queryByChoose(@Param("vo") SysRoleDTO sysRoleDTO);
+
+//    foreach标签 批量插入
+    public int insertBatchByForeach(@Param("list") List<SysRole> list);
+
+//    foreach标签 批量删除  （in）
+    public int delByBatchForeach(@Param("ids") String[] ids);
+
+//    foreach标签的 批量删除(or方式)
+    public int delByBatchForeachOr(@Param("ids") String[] ids);
 }
--- zhy-mybatis/zhy-mybatis-dynamic-sql/src/test/java/com/jdjm/MyTest.java
+++ zhy-mybatis/zhy-mybatis-dynamic-sql/src/test/java/com/jdjm/MyTest.java
@@ -7,6 +7,7 @@ import com.jdjm.vo.SysRoleDTO;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyTest {
@@ -35,5 +36,71 @@ public class MyTest {
 //        List<SysRole> sysRoles = mapper.queryRole(new SysRoleDTO("", "", ""));
         List<SysRole> sysRoles = mapper.queryRole(new SysRoleDTO("管理员", "", null));
         sysRoles.forEach(System.out::println);
+    }
+
+
+    @Test
+    public void testUpdateBySet(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSqlMapper mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+        SysRole sysRole = new SysRole("1695366190980997125",null,"SYSTEM",
+                null,null,null,null);
+        Boolean aBoolean = mapper.updateBySet(sysRole);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void testQueryByChoose(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSqlMapper mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+        SysRoleDTO sysRoleDTO = new SysRoleDTO("", "SYSTEM", null);
+//        SysRoleDTO sysRoleDTO = new SysRoleDTO("管理员", "SYSTEM", null);
+        List<SysRole> sysRoles = mapper.queryByChoose(sysRoleDTO);
+        sysRoles.forEach(sysRole -> System.out.println(sysRole));
+    }
+
+//    foreach标签 批量删除
+    @Test
+    public void testdelByBatchForeach(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSqlMapper mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+        String[] ids = {"1695366190980997129","1695366190980997130","1695366190980997131"};
+        int count = mapper.delByBatchForeach(ids);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void testinsertBatchByForeach(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSqlMapper mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+        List<SysRole> list = new ArrayList<>();
+        SysRole sysRole1 = new SysRole();
+        sysRole1.setRoleName("徐晃来也");
+        sysRole1.setRoleCode("SYSTEM");
+
+        SysRole sysRole2 = new SysRole();
+        sysRole2.setRoleCode("COMMON");
+        sysRole2.setRoleName("麻花");
+
+        list.add(sysRole1);
+        list.add(sysRole2);
+
+        int i = mapper.insertBatchByForeach(list);
+        sqlSession.commit(); sqlSession.close();
+    }
+
+
+
+    @Test
+    public void testdelByBatchForeachOr(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSqlMapper mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+        String[] ids = {"1695366190980997129","1695366190980997130","1695366190980997131"};
+        int count = mapper.delByBatchForeachOr(ids);
+        sqlSession.commit();
+        sqlSession.close();
     }
 }
